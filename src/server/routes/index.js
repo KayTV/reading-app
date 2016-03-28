4d6cc4426be7a42db144b2002fd40d401b1c027d
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-03-28 15:09:47 -0600

    edit pages completed

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -14,6 +14,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Galvanize Reads' });
 });
 
+router.get('/addbook', function(req, res, next) {
+  res.render('addbook', { title: 'Galvanize Reads: Add Book' });
+});
+
 router.get('/books', function(req, res, next) {
   books().select()
   .then(function(books){
@@ -39,5 +43,33 @@ router.get('/showpageauthor/:id', function(req, res, next) {
     res.render('showpageauthor', {user: req.user, author: author});
   });
 })
+
+router.get('/editbook/:id', function(req, res, next) {
+  books().where('id', req.params.id)
+    .then(function (book) {
+      res.status(200)
+        res.render('editbook',{
+          book: book[0],
+          user: req.user,
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+});
+
+router.get('/editauthor/:id', function(req, res, next) {
+  authors().where('id', req.params.id)
+    .then(function (author) {
+      res.status(200)
+        res.render('editauthor',{
+          author: author[0],
+          user: req.user,
+        });
+    })
+    .catch(function (err) {
+      return next(err);
+    });
+});
 
 module.exports = router;
