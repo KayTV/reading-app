6a8a2c2cd5b9840ce33f56fe4ae331667290bd52
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-03-28 11:40:43 -0600

    basic routes

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -5,4 +5,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Galvanize Reads' });
 });
 
+router.get('/books', function(req, res, next) {
+  res.render('books', { title: 'Galvanize Reads: Books' });
+});
+
+router.get('/authors', function(req, res, next) {
+  res.render('authors', { title: 'Galvanize Reads: Authors' });
+});
+
 module.exports = router;
