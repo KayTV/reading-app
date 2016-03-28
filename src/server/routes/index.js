7ff5abe221bc2861bea3246f2f69cb1e84ee8fea
Kaylyn Van Norstrand <kaylyn.vannorstrand@gmail.com>
2016-03-28 12:01:28 -0600

    html books and author

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,5 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../../../db/knex');
+function authors() {
+  return knex('author');
+}
+function books() {
+  return knex('books');
+}
+
+
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Galvanize Reads' });
